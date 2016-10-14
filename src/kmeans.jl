81f0126e0295e9062d4b1520954cab5e02000d2e
lbuser <lbuser@noreply.github.com>
2016-10-14 12:05:07 -0400

    z patients

--- src/kmeans.jl
+++ src/kmeans.jl
@@ -4,9 +4,9 @@ using DataFrames
 include("loadData.jl")
 
 
-function runKmeans(n_clusters=4, data=loadPatients())
+function runKmeans(;n_clusters=4, data=loadLogPatients())
   rois = loadRoiCols()
-  kmeans(Matrix(data[rois])', n_clusters)
+  kmeans(Matrix(data[rois])', n_clusters, display=:final)
 end
 
 
@@ -18,15 +18,18 @@ function centersDf(y::KmeansResult)
   @assert length(rois) == size(centers, 1)
 
   ret = DataFrame(roi = rois)
+
+  is_min = centers .== minimum(centers, 2) + 0.
   for c in 1:size(centers, 2)
     ret[Symbol("mean_", c)] = centers[:, c]
+    ret[Symbol("is_min_", c)] = is_min[:, c]
   end
 
   ret
 end
 
 
-function assignmentsDf(y::KmeansResult, rids=loadPatients()[:RID])
+function assignmentsDf(y::KmeansResult=runKmeans(), rids=loadLogPatients()[:RID])
   assignments = y.assignments
 
   @assert length(rids) == length(assignments)
--- src/loadData.jl
+++ src/loadData.jl
@@ -11,7 +11,6 @@ function loadRawData(raw_f)
 end
 
 
-
 function loadData()
   data = readtable("../data/input_data_nas.csv")
 
@@ -26,7 +25,7 @@ function loadData()
 end
 
 
-function loadNormalizedData()
+function loadNormalizedData(normalize_fn::Function)
   data = loadData()
 
   rois = loadRoiCols()
@@ -34,14 +33,48 @@ function loadNormalizedData()
   normalized_data = copy(data)
 
   for r in rois
-    normalized_data[r] = data[r] ./ data[:BL_ICV]
+    normalized_data[r] = normalize_fn(r, data[r], data[:BL_ICV])
   end
 
   normalized_data
 end
 
 
-function loadPatients()
-  data = loadNormalizedData()
+loadLogNormalizedData() = loadNormalizedData((x, r, b) -> log(r) - log(b))
+loadDivNormalizedData() = loadNormalizedData((x, r, b) -> r ./ b)
+load1000NormalizedData() = loadNormalizedData((x, r, b) -> r ./ (1e-3 * b))
+
+
+function loadPatients(fn::Function = loadData)
+  data = fn()
   data[data[:BL_DX_coded] .== 1, :]
+end
+
+
+function loadControls(fn::Function = loadData)
+  data = fn()
+  data[data[:BL_DX_coded] .== 0, :]
+end
+
+
+loadDivPatients() = loadPatients(loadDivNormalizedData)
+loadLogPatients() = loadPatients(loadLogNormalizedData)
+load1000Patients() = loadPatients(load1000NormalizedData)
+
+loadDivControls() = loadControls(loadDivNormalizedData)
+loadLogControls() = loadControls(loadLogNormalizedData)
+
+function loadZscorePatients()
+  data_controls = loadDivControls()
+  rois = loadRoiCols()
+
+  data_patients = loadDivPatients()
+  data_patients_z = copy(data_patients)
+
+  for r in rois
+    mn, standard = mean_and_std(data_controls[r])
+    data_patients_z[r] = (data_patients_z[r] .- mn) ./ standard
+  end
+
+  data_patients_z
 end
